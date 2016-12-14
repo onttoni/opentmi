264f071613b325a5a1102fcdec4a5477dd3cf0d8
Eero Paavola <paavola.ee@gmail.com>
2016-12-14 16:22:54 +0200

    Refactor dbdump.sh, add dbrestore.sh.

--- db_backup/dbrestore.sh
+++ db_backup/dbrestore.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-# Helper script that creates a timestamped database dump folder to the same directory as the script.
+# Helper script that restores database collections from a db dump taken with mongodump (=dbdump.sh).
 # USAGE
-# localhost db: $ ./dbdump.sh local
-# remote db:    $ ./dbdump.sh remote
+# local: $ ./dbrestore.sh local <pathOfDumpDirToRestore>
+# remote: $ ./dbrestore.sh remote <pathOfDumpDirToRestore>
 #
-#https://docs.mongodb.com/v3.2/reference/program/mongodump/
+#https://docs.mongodb.com/v3.2/reference/program/mongorestore/
 
 
 ###########################################################
@@ -30,44 +30,40 @@ REMOTE_DB_PASSWORD="i50lrigs2ki7638kjdffnnaid9"
 #############################################################
 
 function printHelp {
-    echo "USAGE: ./dbdump.sh ['local' | 'remote']"
+    echo "USAGE: ./dbrestore.sh ['local' | 'remote'] <pathOfDumpToRestore>"
     echo "'local' are 'remote' are aliases for host, port, dbname."
     echo "To change the values for local and remote, edit the variables in the script."
 }
 
 function printDumpInfo {
-    echo "Running mongodump with following settings:"
+    echo "Running mongorestore with following settings:"
     echo "hostaddress: $MONGO_HOST"
     echo "port: $MONGO_PORT"
     echo "db name: $DB_NAME"
 }
 
-if [[ ( "$#" -ne 1 ) || ( "$1" -ne "local" ) || ( "$1" -ne "remote" ) ]]; then
+if [[ ( "$#" -ne 2 ) || ( "$1" -ne "local" ) || ( "$1" -ne "remote" ) ]]; then
     printHelp
     exit 1
 fi
-
 # change working dir to location of script
 cd "$(dirname "$0")"
+DB_DIR="$2"
 
-APP_NAME="inventory"
-TIMESTAMP=`date +%Y%m%dT%H%M%S`
-DEST=./${APP_NAME}-dump_${TIMESTAMP}
-mkdir $DEST
-
-if [ "$1" = "local" ]; then
+if [ "$1" = "local" ] && [ -d "$2" ]; then
     MONGO_HOST="$LOCAL_MONGO_HOST"
     MONGO_PORT="$LOCAL_MONGO_PORT"
     DB_NAME="$LOCAL_DB_NAME"
     printDumpInfo
-    mongodump -h $MONGO_HOST:$MONGO_PORT -d $DB_NAME -o $DEST
-elif [ "$1" = "remote" ]; then
+    mongorestore -h $MONGO_HOST:$MONGO_PORT -d $DB_NAME $DB_DIR --drop
+elif [ "$1" = "remote" ] && [ -d "$2" ]; then
     MONGO_HOST="$REMOTE_MONGO_HOST"
     MONGO_PORT="$REMOTE_MONGO_PORT"
     DB_NAME="$REMOTE_DB_NAME" 
     printDumpInfo
     echo "db username: $REMOTE_DB_USERNAME"
-    mongodump -h $MONGO_HOST:$MONGO_PORT -d $DB_NAME -u $REMOTE_DB_USERNAME -p $REMOTE_DB_PASSWORD -o $DEST
+    mongorestore -h $MONGO_HOST:$MONGO_PORT -d $DB_NAME -u $REMOTE_DB_USERNAME -p $REMOTE_DB_PASSWORD $DB_DIR --drop
 else
+    echo "Check input db path !!!"
     printHelp
 fi
